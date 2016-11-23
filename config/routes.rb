17b7b8a3eb50d5eb36d5638ef958296859d718e6
amitsaxena <amit@mazdigital.com>
2016-11-24 02:32:59 +0530

    A test HTML page.

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   root to: 'pages#index'
   post 'result', to: 'pages#result'
   get 'result', to: redirect('/')
+  get '8f90b5a2-186d-47d7-b72b-b7ed0eee7ce6', to: 'pages#sample'
   
 end
